e9d73661ff2def838f2b104735d197ec8c3067e3
saman-akhtar <33394840+saman-akhtar@users.noreply.github.com>
2019-09-16 00:50:39 +0530

    Add files via upload

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
-import { take } from 'rxjs/operators';
+import { take, map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
 
@@ -14,12 +14,19 @@ export class AdminProductsComponent implements OnInit , OnDestroy {
 products: Product[];
 subscription: Subscription;
 filteredProducts: any[];
+  //  this.subscription = this.prodService.getAll().valueChanges().
+  //  subscribe(products => this.filteredProducts = this.products  = products);
+  //   console.log('In Admin prod com, val of product$:', this.products);
+  // }
   constructor(private prodService: ProductService) {
-   this.subscription = this.prodService.getAll().valueChanges().
-   subscribe(products => {
-     this.filteredProducts = this.products  = products;
+
+    this.filteredProducts = this.products = this.prodService.getAll().snapshotChanges().pipe(
+    map(products => {return products.map(product => {
+      const key = product.key;
+      const value = product.payload.val();
+      return {key, ...value};
     });
-    console.log('In Admin prod com, val of product$:', this.products);
+   }));
   }
 filter(query) {
   console.log('In Admin prod com, val of query:', query);
